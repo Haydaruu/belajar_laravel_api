3b289a80c07572248a7250303272547d512816f8
Haydaruu <falorfalconin@gmail.com>
2023-10-18 16:12:08 +0700

    feature: crud users

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Ahmad Haydar',
+            'email' => 'haydaru@gmail.com',
+            'password' => bcrypt('1234'),
+            'is_admin' => true
+        ]);
+        User::factory()->count(50)->create();
+    }
+}
