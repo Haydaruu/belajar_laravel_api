d37e435fe523dd68814e6d63c44eb061e703bade
Haydaruu <falorfalconin@gmail.com>
2023-10-20 10:23:14 +0700

    add complete crud task

--- app/Http/Controllers/API/TaskController.php
+++ app/Http/Controllers/API/TaskController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Task\TaskStoreRequest;
 use App\Http\Resources\Task\TaskIndexResource;
+use App\Http\Resources\Task\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -37,9 +39,8 @@ class TaskController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(TaskStoreRequest $request)
     {
-
         $file = $request->file('upload_file')->getClientOriginalExtension();
 
         if (in_array($file, ['jpg', 'png', 'jpeg'])) {
@@ -54,10 +55,12 @@ class TaskController extends Controller
             'user_id' => auth()->user()->id,
         ]);
 
+        $task = Task::create($request->validated());
+
         return response()->json([
             'status' => true,
             'message' => 'Berhasil menambahkan task',
-            'data' => $task,
+            'data' => new TaskResource($task),
 
         ], Response::HTTP_CREATED);
     }
@@ -65,24 +68,55 @@ class TaskController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Task $task)
     {
-        //
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil menampilkan task',
+            'data' => new TaskResource($task),
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Task $task)
     {
-        //
+        $file = $request->file('upload_file')->getClientOriginalExtension();
+
+        $upload_file = null;
+        if ($request->hasFile('upload_file')) {
+            $file = $request->file('upload_file')->getClientOriginalExtension();
+
+            if (in_array($file, ['jpg', 'png', 'jpeg'])) {
+                $upload_file = $request->file('upload_file')->store('public/images');
+            } else {
+                $upload_file = $request->file('upload_file')->store('public/files');
+            }
+        }
+
+        $task->update([
+            'title' => $request->get('title'),
+            'description' => $request->get('description'),
+            'upload_file' => $upload_file,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Berhasil mengubah task',
+            'data' => new TaskResource($task),
+
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
+        $task->delete();
 
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
